7fef7a35f93869a78061b87c508e09ba43ab51cf
Hue-Ray <kunletobi4@gmail.com>
2023-04-11 18:28:57 +0100

    bug fix

--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import '../styles/style.css';
 import '../styles/mordern-normalize.css';
 import '../styles/components/header.css';
 import '../styles/components/hero-section.css';
+import '../styles/components/services.css';
 import '../styles/utils.css';
