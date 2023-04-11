c34970bc98fd837c58e2b76fd4c02d6f71d75ed3
Hue-Ray <kunletobi4@gmail.com>
2023-04-11 16:16:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# photo-studio-website-vite-project-
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import '../styles/style.css';
+import '../styles/mordern-normalize.css';
+import '../styles/components/header.css';
+import '../styles/components/hero-section.css';
+import '../styles/utils.css';
