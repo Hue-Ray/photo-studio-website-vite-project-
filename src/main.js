2a2d7f0916203cbb3b42566daf2060b808a3ecac
Hue-Ray <kunletobi4@gmail.com>
2023-04-12 19:38:43 +0100

    finished mobile view

--- src/main.js
+++ src/main.js
@@ -4,4 +4,5 @@ import '../styles/components/header.css';
 import '../styles/components/hero-section.css';
 import '../styles/components/services.css';
 import '../styles/components/gallery.css';
+import '../styles/components/contact.css';
 import '../styles/utils.css';
